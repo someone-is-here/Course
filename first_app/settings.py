dd5523c908fee0278425e48586fade5da301f6ae
Your Name <you@example.com>
2023-01-24 11:52:28 +0300

    Modify settings.py

--- first_app/settings.py
+++ first_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_z!2^pjj&a=bx55_^7p%=3w09bozy2-qlq0mbydrsua-+b+!er
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['localhost', '0.0.0.0','web-production-415d.up.railway.app'] # for docker compose '0.0.0.0'
+ALLOWED_HOSTS = ['localhost', '0.0.0.0','web-production-415d.up.railway.app', '127.0.0.1'] # for docker compose '0.0.0.0'
 
 # Application definition
 
@@ -104,12 +104,9 @@ WSGI_APPLICATION = 'first_app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'learningdb',
-    }
+     'default': dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/learningdb',
+                                       conn_max_age=600)
 }
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -159,6 +156,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'Course/static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
